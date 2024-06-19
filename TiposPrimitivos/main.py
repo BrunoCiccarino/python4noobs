07ffe2f1ef224ebff25414e32fd428ef7a041d97
Bruno Ciccarino λ <82242552+BrunoCiccarino@users.noreply.github.com>
2024-06-19 19:50:57 -0300

    .

---
+++ TiposPrimitivos/main.py
@@ -0,0 +1,25 @@
+# Tipos primitivos no python
+# Tipo inteiro -> integer -> int 
+# Pode ser qualquer numero inteiro
+# Tanto faz se é negativo ou positivo
+print(1)
+print(-1)
+
+# Tipo flutuante float
+# representa números com pontos flutuante
+# pode ser qualquer numero basta ter esse ponto '.'
+print("1.345")
+print("-563.44")
+
+# Tipo booleano -> True or False
+# Expressões booleanas necessariamente retornam true ou false
+# Podem ser utilizados dentro de laços de repetição
+# i = 748
+# if i == 890:
+#     print("O número escolhido é igual o numero especificado")
+# else:
+#     print("O numero especificado é diferente")
+
+# A classe type mostra o tipo que esta sendo usado!
+i = 5
+print(type(i))
