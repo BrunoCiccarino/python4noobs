06bb1c953f1d8a084053cf8e50a9b2907e332f3c
Bruno Ciccarino λ <82242552+BrunoCiccarino@users.noreply.github.com>
2024-06-20 17:29:17 -0300

    .

---
+++ Exercicios/main.py
@@ -0,0 +1,10 @@
+nota1 = int(input("Digite a primeira nota do aluno: "))
+nota2 = int(input("Digite a segunda nota do aluno: "))
+
+media = (nota1 + nota2) / 2
+
+if media > 6:
+    print("Aluno aprovado")
+
+else:
+    print("Aluno reprovado")
